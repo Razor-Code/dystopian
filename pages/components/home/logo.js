d58659ab05b14c9a2c357f35ffcd9834fc4b1363
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-08-06 17:44:05 +0530

    title screen & logo

---
+++ pages/components/home/logo.js
@@ -0,0 +1,36 @@
+export default function Logo() {
+  return (
+    <svg className="logo" width="540" height="143" viewBox="-150 0 840 143" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="dystopyan">
+    <g id="n">
+    <path id="Vector 9" d="M458 135L494 50L525 120L536 35L512 25" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <circle id="Ellipse 3" cx="501" cy="22" r="4" fill="currentColor"/>
+    </g>
+    <g id="a">
+    <path id="Vector 8" d="M464.333 77L413 118L429.667 17L464.333 77ZM464.333 77L422.076 63M464.333 77L483 118" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <circle id="Ellipse 4" cx="428" cy="4" r="4" fill="currentColor"/>
+    </g>
+    <g id="i">
+    <circle id="Ellipse 4_2" cx="382.5" cy="63" r="4" fill="currentColor"/>
+    <path id="Vector 10" d="M382.5 125V88" stroke="currentColor" strokeWidth="8" strokeLinecap="round"/>
+    </g>
+    <g id="y2">
+    <circle id="Ellipse 3_2" cx="396.5" cy="51" r="4" fill="currentColor"/>
+    <path id="y2_2" d="M355 50L382.5 89M382.5 89L400.5 63H382.5M382.5 89V124" stroke="white" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    </g>
+    <path id="p" d="M332.667 43.7629V79.8814M332.667 116V79.8814M332.667 79.8814L378 32.5052L310 25" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <path id="o" d="M243 82L264.56 47H306.14L320 82L300.75 124H264.56L243 82Z" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <path id="t" d="M183 15L222 22.4539M261 29.9079L222 22.4539M222 22.4539V118" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <g id="s">
+    <path id="Vector 3" d="M171 14L133 45L171 86L133 130" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <circle id="Ellipse 2" cx="124" cy="139" r="4" fill="currentColor"/>
+    </g>
+    <g id="y1">
+    <path id="Vector 2" d="M75 22L106 61M106 61L123 34M106 61V109" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    <circle id="Ellipse 1" cx="128" cy="25" r="4" fill="currentColor"/>
+    </g>
+    <path id="d" d="M12 44V130L81 66L4 21" stroke="currentColor" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+    </g>
+    </svg>
+  )
+}
