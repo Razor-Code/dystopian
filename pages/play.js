d4e59774afce34c6ce4922c4342dd0a73a50a481
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-08-12 19:19:10 +0530

    added basic files

--- pages/play.js
+++ pages/play.js
@@ -1,10 +1,123 @@
-import DialogueBox from "../components/DialogueBox/dialogueBox"
+import { useEffect, useRef, useState } from "react";
+import Game from "./Game";
+import SpriteSheets from "./data/SpriteSheets";
+import SpriteSheetManager from "./SpriteSheetManager";
+import SpriteSheet from "./SpriteSheet";
 
-export default function() {
+function App() {
+  var canvasRef = useRef();
+  var [menu, setMenu] = useState(false);
+  var gameRef = useRef();
+
+  useEffect(() => {
+    var ssm = new SpriteSheetManager();
+
+    Object.keys(SpriteSheets).forEach((spriteSheetName) => {
+      var data = SpriteSheets[spriteSheetName];
+      ssm.addSpriteSheet(spriteSheetName, data.imgPath, data);
+    });
+
+    var canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    
+    var ctx = canvas.getContext("2d");
+
+    var last = performance.now();
+
+    var game = new Game(canvas, ctx, ssm);
+    gameRef.current = game;
+
+    window.game = game;
+
+    var cb = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      canvas.style.width = window.innerWidth + "px";
+      canvas.style.height = window.innerHeight + "px";
+
+      game.camera.updateAspectRatio();
+    };
+    cb();
+    var active = true;
+    game.active = true;
+
+    function update() {
+      var now = performance.now();
+      var delta = (now - last) / 1000;
+      last = now;
+
+      if (delta > 0.25) {
+        delta = 0.25;
+      }
+
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      if (game.active) {
+        game.updateGame(delta);
+      }
+      game.renderGame(delta);
+
+      active && requestAnimationFrame(update);
+    }
+
+    ssm.onLoaded = () => {
+      Object.keys(ssm.spriteSheets).forEach((key) => {
+        if (key.includes("player_")) {
+          ssm.spriteSheets[key].getEffects(["flipHorizontally"]);
+        }
+      });
+
+      update();
+
+      setTimeout(() => {
+        game.start();
+        // setMenu(true);
+      }, 100);
+    };
+
+    window.addEventListener("resize", cb);
+
+    return () => {
+      cancelAnimationFrame(update);
+      window.removeEventListener("resize", cb);
+
+      active = false;
+
+      game.end();
+    };
+  }, []);
+
   return (
-    <main>
-      <h1>Dialogue Box Testing</h1>
-      <DialogueBox text="Hey Player! Welcome to Dystopyan, ready to learn Python with us?" image={"/creators/pranshu.png"} />
-    </main>
-  )
+    <div className={!menu ? "hide-cursor" : ""}>
+      <div className={"main-menu" + (menu ? " main-menu-show" : "")}>
+        <h1>Platformer</h1>
+        <p>by: MichaelXF</p>
+
+        <button
+          className='main-btn'
+          onClick={() => {
+            // Start game
+            setMenu(false);
+          }}
+        >
+          Play
+        </button>
+      </div>
+
+      <canvas ref={canvasRef} />
+
+      <div className='controls'>
+        <kbd>W</kbd>
+        <kbd>A</kbd>
+        <kbd>S</kbd>
+        <kbd>D</kbd>
+      </div>
+    </div>
+  );
 }
+
+export default App;
