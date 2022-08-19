b30ba31758e75d5d74e800bb83d6a408376e575f
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-08-19 09:06:53 +0530

    cloned game and updated style

--- pages/play.js
+++ pages/play.js
@@ -1,10 +1,7 @@
-import DialogueBox from "../components/DialogueBox/dialogueBox"
+import App from '../engine/App';
 
 export default function() {
   return (
-    <main>
-      <h1>Dialogue Box Testing</h1>
-      <DialogueBox text="Hey Player! Welcome to Dystopyan, ready to learn Python with us?" image={"/creators/pranshu.png"} />
-    </main>
+    <App />
   )
 }
