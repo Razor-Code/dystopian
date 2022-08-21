0884ee4947597ce6404a83350a11e1e9982fdfd5
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2022-08-22 01:36:37 +0530

    display game objective

--- pages/play.js
+++ pages/play.js
@@ -1,7 +1,27 @@
+import { React, useState, useEffect } from "react";
 import App from '../engine/App';
 
 export default function() {
+  const [displayPopup, setDisplayPopup] = useState(true);
+
+  useEffect(() => {
+    if (displayPopup) {
+      setTimeout(() => {
+        setDisplayPopup(false);
+      } , 3000);
+    }
+  }, [])
+
   return (
-    <App />
+    <>
+      {displayPopup && (
+        <div className="popup">
+          <span className="popup-heading">OBJECTIVE:</span>
+          <span className="popup-description">Find and eliminate the Xenobug</span>
+          {/* <span className="popup-hint">Hint: Xenobugs are often located at the highest level of the hive structure</span> */}
+        </div>
+      )}
+      <App />
+    </>
   )
 }
